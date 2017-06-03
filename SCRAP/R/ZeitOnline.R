87d79261d706ae67602ed5b250369f793c845ee2
Tony Ramos <ramosaj@usc.edu>
2017-06-03 15:16:02 -0700

    started function to get ZeitOnline articles

---
+++ SCRAP/R/ZeitOnline.R
@@ -0,0 +1,11 @@
+scrape.ZeitHeadlines <- function()
+{
+  require(rvest)
+  titles_nodes <- html_text(doc,".teaser-small__combined-link, .teaser-fullwidth__media-link, .teaser-large__combined-link")
+  titles <- xml_attr(title_nodes,"title")
+  links <- xml_attr(title_nodes,"title")
+  Titles.Links <- data.frame(titles=titles,links=links)
+  return(Titles.Links)
+}
+
+
