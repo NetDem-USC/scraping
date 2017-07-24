9f86c3bac6dccc89a9ae5dac3baa506b380392a9
Tony Ramos <ramosaj@usc.edu>
2017-07-24 13:58:17 -0700

    Edited SpiegelOnline.R

--- SCRAP/R/SpiegelOnline.R
+++ SCRAP/R/SpiegelOnline.R
@@ -7,21 +7,48 @@ scrape.SpiegelOnline <- function() {
 
   require(rvest)
   doc <-read_html("http://www.spiegel.de/")
-  #get main article
-  doc %>% html_nodes(".hp-first-article a")[1] %>% xml_attr("href")
-  doc %>% html_nodes(".hp-first-article a")[1] %>% xml_attr("title")
-
-  doc %>% html_nodes(".column-wide h2 a") %>% xml_attr("href")
-  doc %>% html_nodes(".column-wide h2 a") %>% xml_attr("title")
+  #get main articles
+  title=html_nodes(doc,".article-title a")
+  titles = xml_attr(title,"title")
+  title_links = xml_attr(title,"href")
+  df <- data.frame(title=titles, url=title_links, 
+                   time=as.character(Sys.time()), stringsAsFactors=F)
+  df <- df[!is.na(df$url),]
+  df <- df[!duplicated(df$url),]
+  
+  return(df)
 }
 
 #'@title scrapes an article off of spiegel.com
 #'@description Scrapes article date, content, and summary from spiegel article
 #'@param url string of article url
 scrape.SpiegelArticle <- function(url) {
+  
   article <- read_html(url)
-  date <- html_nodes(".article-function-date")
-  content <- html_text(html_nodes(".spArticleContent p"))
+  article_intro <- html_text(html_nodes(article,".headline-intro"))[1]
+  article_title <- html_text(html_nodes(article,".headline"))[1]
+  article_title <- paste(article_intro,article_title,sep=": ")
+  
+  date <- html_text(html_nodes(article,".article-function-date"))[1]
+  date <- gsub("\t", "", gsub("\r", "", date))
+  
+  content <- html_text(html_nodes(article,"p"))
   summary <- content[1]
+  
+  content <- paste(content, collapse="\n\n")
+  content <- gsub("^ *| *$", "", gsub("\n|\t|\r", "", content))
+  
+  summary <- gsub("^ *| *$", "", gsub("\n", "", summary))
+  summary <- gsub(" {2,}", " ", summary)
+  
+  comments = grep("insgesamt (.*) Beiträge",html_text(html_nodes("span"),value=TRUE))
+  
+  
+  article_df <- data.frame(date=date,summary=summary,
+                           headline=article_title,text=content,
+                           comments=ifelse(length(comments)==0, NA, comments)
+                           stringsAsFactors=F)
+                           
+  return(article_df)
 
 }
